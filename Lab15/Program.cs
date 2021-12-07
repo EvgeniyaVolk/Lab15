484569b05d3d5ba22f11c4083723b00658b618c3
Волкова Евгения < @GENYADELL>
2021-12-07 23:02:13 +0300

    Добавьте файлы проекта.

---
+++ Lab15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        interface ISeries
+        {
+            void setStart(int x);//Устанавливает начальное значение
+            int getNext();//Возвращает следующее число ряда
+            void reset();//Выполняет сброс к начальному значению
+        }
+        class ArithProgression : ISeries
+        {
+            public void setStart(int x)
+            {
+                throw new NotImplementedException();
+            }
+            public int getNext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void reset()
+            {
+                throw new NotImplementedException();
+            }
+
+           
+        }
+        class GeomProgression : ISeries
+        {
+            public void setStart(int x)
+            {
+                throw new NotImplementedException();
+            }
+            public int getNext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
